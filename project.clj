09dc6283ce8fec531a2bc42a9768d8bcbd6a0272
Brenton Ashworth <bashwort@gmail.com>
2010-05-28 17:03:04 -0700

    Move code from sandbar into this project, rename files and get unit tests working.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject carte/carte "0.0.1-SNAPSHOT"
+  :description "Relational Persistence for Clojure."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [inflections "0.3"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [jline "0.9.94"]
+                     [mysql/mysql-connector-java "5.1.6"]])
